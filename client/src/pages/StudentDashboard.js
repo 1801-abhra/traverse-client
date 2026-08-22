0673d10d49362aef662ca208acf3e8626061cdc1
1801-abhra <251030034@juitsolan.in>
2026-08-22 20:39:38 +0530

    Fix dark map with visible labels

--- client/src/pages/StudentDashboard.js
+++ client/src/pages/StudentDashboard.js
@@ -516,8 +516,8 @@ function StudentDashboard() {
                     style={{ height: '300px', borderRadius: '12px' }}
                   >
                     <TileLayer
-                      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                      url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
                     />
 
                     {/* Full route dashed line */}
@@ -781,8 +781,8 @@ function StudentDashboard() {
                     style={{ height: '220px', borderRadius: '12px' }}
                   >
                     <TileLayer
-                      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                      url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
                     />
                     <Marker position={JUIT_COORDS}>
                       <Popup>📍 JUIT Campus (Pickup)</Popup>
